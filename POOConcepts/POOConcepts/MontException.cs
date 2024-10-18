8cf68f8f2cb8ce93a34669aec8e7df837a7ddc95
Victor Mendoza <vmendozaespina@gmail.com>
2024-10-18 09:34:23 -0500

    finishing class date

---
+++ POOConcepts/POOConcepts/MontException.cs
@@ -0,0 +1,19 @@
+﻿
+namespace POOConcepts
+{
+    [Serializable]
+    internal class MontException : Exception
+    {
+        public MontException()
+        {
+        }
+
+        public MontException(string? message) : base(message)
+        {
+        }
+
+        public MontException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ POOConcepts/POOConcepts/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+using POOConcepts;
+
+Console.WriteLine("POO Concepts");
+Console.WriteLine("============");
+
+try
+{
+	Console.WriteLine(new Date(2024, 2, 29));
+	Console.WriteLine(new Date(1984, 3, 29));
+	Console.WriteLine(new Date(1986, 9, 7));
+    Console.WriteLine(new Date(2010, 11, 30));
+}
+catch (Exception erro)
+{
+	Console.WriteLine(erro.Message);
+}
